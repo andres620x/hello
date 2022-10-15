baf68b3e416b116cf0ddbd4eb08a1dfe965f8605
Jorge Andres Garcia Zapata <joranzap@bancolombia.com.co>
2022-10-15 00:59:37 -0500

    ajuste boostrap

--- www/js/home.js
+++ www/js/home.js
@@ -25,7 +25,6 @@ function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
 }
 
 function callAnothePage()
@@ -42,3 +41,12 @@ btnBack.onclick = function () {
     alert('hola');
     window.location = "index.html";
 }
+
+
+var menu_btn = document.querySelector("#menu-btn");
+var sidebar = document.querySelector("#sidebar");
+var container = document.querySelector(".my-container");
+menu_btn.addEventListener("click", () => {
+    sidebar.classList.toggle("active-nav");
+    container.classList.toggle("active-cont");
+});
